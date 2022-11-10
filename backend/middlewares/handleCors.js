f8d283ff1f7772b8b736cbe058475329f646e0f4
Gudkova Mariia <gudmary8@gmail.com>
2022-11-10 11:45:17 +0400

    change cors middleware in back

--- backend/middlewares/handleCors.js
+++ backend/middlewares/handleCors.js
@@ -1,12 +1,10 @@
 const { allowedCors, DEFAULT_ALLOWED_METHODS } = require('../utils/constants');
 
 // eslint-disable-next-line consistent-return
-function handleCors(req, res, next) {
+function handleCors(req, res) {
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
   }
-
-  next();
 
   const { method } = req;
   const requestHeaders = req.headers['access-control-request-headers'];
