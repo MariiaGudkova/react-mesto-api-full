0e5b53ce39a7eb35c5204398a4a7f47322ef26c3
Gudkova Mariia <gudmary8@gmail.com>
2022-11-10 11:58:30 +0400

    change cors middleware in back

--- backend/middlewares/handleCors.js
+++ backend/middlewares/handleCors.js
@@ -5,6 +5,7 @@ function handleCors(req, res) {
   const { origin } = req.headers;
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
+    return res.end();
   }
 
   const { method } = req;
